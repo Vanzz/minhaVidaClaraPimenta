2907459bc69b4beae4c7c7921da246e8ef4894fb
Vanzz <tiivansan@hotmail.com>
2015-09-08 21:25:14 -0300

    SéBurroÉ

---
+++ AprendendoAJogarVelha/src/Muriel/PlayerJogada.java
@@ -0,0 +1,13 @@
+package Muriel;
+import javax.swing.JOptionPane;
+
+public class PlayerJogada {
+	
+	public No playerJogada(No pTabuleiro)
+	{
+		String[][] m = pTabuleiro.matriz;
+		
+		
+		return null;
+	}
+}
